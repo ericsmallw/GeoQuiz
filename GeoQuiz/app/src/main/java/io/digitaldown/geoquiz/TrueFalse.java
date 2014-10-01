3b7e4e6f1210b3a2fc7997e229e878a5ae8dd840
unknown <esmallwood@SW221642.ellis.com>
2014-09-30 20:57:27 -0500

    Finished Chapter 2, still need to do extra exercises

---
+++ GeoQuiz/app/src/main/java/io/digitaldown/geoquiz/TrueFalse.java
@@ -0,0 +1,31 @@
+package io.digitaldown.geoquiz;
+
+/**
+ * Created by esmallwood on 9/30/2014.
+ */
+public class TrueFalse {
+    private int mQuestion;
+
+    private boolean mTrueQuestion;
+
+    public TrueFalse(int question, boolean trueQuestion) {
+        mQuestion = question;
+        mTrueQuestion = trueQuestion;
+    }
+
+    public int getQuestion() {
+        return mQuestion;
+    }
+
+    public void setQuestion(int question){
+        mQuestion = question;
+    }
+
+    public boolean isTrueQuestion() {
+        return mTrueQuestion;
+    }
+
+    public void setTrueQuestion(boolean trueQuestion){
+        mTrueQuestion = trueQuestion;
+    }
+}
--- GeoQuiz/app/src/main/java/io/digitaldown/geoquiz/geoquiz.java
+++ GeoQuiz/app/src/main/java/io/digitaldown/geoquiz/geoquiz.java
@@ -6,6 +6,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -13,6 +14,18 @@ public class geoquiz extends Activity {
 
     private Button mTrueButton;
     private Button mFalseButton;
+    private TextView mQuestionTextView;
+    private Button mNextButton;
+
+    private TrueFalse[] mQuestionBank = new TrueFalse[]{
+            new TrueFalse(R.string.question_africa, false),
+            new TrueFalse(R.string.question_americas, true),
+            new TrueFalse(R.string.question_asia, true),
+            new TrueFalse(R.string.question_mideast, false),
+            new TrueFalse(R.string.question_oceans, true)
+    };
+
+    private int mCurrentIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,26 +34,43 @@ public class geoquiz extends Activity {
 
         mTrueButton = (Button)findViewById(R.id.true_button);
         mFalseButton = (Button)findViewById(R.id.false_button);
+        mNextButton = (Button)findViewById(R.id.next_button);
+        mQuestionTextView = (TextView)findViewById(R.id.question_text_view);
 
         init();
     }
 
     private void init() {
+        updateQuestion();
+
+        //set events
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(geoquiz.this, R.string.correct_toast, Toast.LENGTH_SHORT);
+                Toast.makeText(geoquiz.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
             }
         });
 
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(geoquiz.this, R.string.incorrect_toast, Toast.LENGTH_SHORT);
+                Toast.makeText(geoquiz.this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                updateQuestion();
             }
         });
     }
 
+    private void updateQuestion(){
+        int question = mQuestionBank[mCurrentIndex].getQuestion();
+        mQuestionTextView.setText(question);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
